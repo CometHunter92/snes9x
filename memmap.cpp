06e1efb3b7ef30103715b7e6252dbff251b449ac
CometHunter92 <83950018+CometHunter92@users.noreply.github.com>
2022-08-31 00:51:47 +0200

    Update memmap.cpp

--- memmap.cpp
+++ memmap.cpp
@@ -3645,7 +3645,7 @@ void CMemory::ApplyROMFixes (void)
 
 	// SRAM value fixes
 	if //(match_na("SUPER DRIFT OUT")      || // Super Drift Out
-		match_na("SATAN IS OUR FATHER!") ||
+		(match_na("SATAN IS OUR FATHER!") ||
 		match_na("goemon 4"))               // Ganbare Goemon Kirakira Douchuu
 		SNESGameFixes.SRAMInitialValue = 0x00;
 
